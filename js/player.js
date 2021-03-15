c466a9bc34992d424589422729a57ed6995d4959
Gary Larson <64063020+theGaryLarson@users.noreply.github.com>
2021-03-14 23:18:51 -0700

    removed leading / in img src paths

--- js/player.js
+++ js/player.js
@@ -6,11 +6,11 @@ class Player {
         this.currentRoom;
         
         //starting items
-        var item0 = new Item("lint", "/img/lint.png", null, "Some pocket lint. You ponder adding it to your belly button lint collection but some may consider it cheating.");
+        var item0 = new Item("lint", "img/lint.png", null, "Some pocket lint. You ponder adding it to your belly button lint collection but some may consider it cheating.");
                 
-        var item1 = new Item("loose change", "/img/change.png", null, "Hmm.. who knows maybe there will be a vending machine around here somewhere.")
+        var item1 = new Item("loose change", "img/change.png", null, "Hmm.. who knows maybe there will be a vending machine around here somewhere.")
 
-        var item2 = new Item("bubble gum", "/img/gum.png", null, "Well, at least it hasn't been chewed. If i get thirsty this might help sate my thirst.")
+        var item2 = new Item("bubble gum", "img/gum.png", null, "Well, at least it hasn't been chewed. If i get thirsty this might help sate my thirst.")
         this.items = [item0, item1, item2]
     }
 }//end player class
